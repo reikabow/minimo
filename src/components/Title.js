2e9e33ecdcb31bb168e6d456f196448ac5a1f10e
Victor Sui <victorsui11@gmail.com>
2017-07-17 23:12:30 -0500

    Style Title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import Button from 'antd/lib/button';
+import { StyleSheet, css } from 'aphrodite';
 
 const Title = props => (
-  <div id="title">
-    <h1>MINIMO</h1>
-    <span>Pit your belongings against each other</span>
-    <span>See how they stack up in utility</span>
-    <span>Minimalize your life</span>
-    <Button autoFocus={ true } onClick={ props.handleClick }>Enter</Button>
+  <div className={ css(styles.page) }>
+    <h1 className={ css(styles.title) }>MINIMO</h1>
+    <span className={ css(styles.span) }>Pit your belongings against each other</span>
+    <span className={ css(styles.span) }>See how they stack up in utility</span>
+    <span className={ css(styles.span) }>Minimalize your life</span>
+    <Button className={ css(styles.button) } autoFocus={ true } onClick={ props.handleClick }>Enter</Button>
   </div>
 );
+
+const styles = StyleSheet.create({
+  page: {
+    textAlign: 'center',
+    margin: '30vw auto',
+    fontSize: '1.35em'
+  },
+
+  title: {
+    display: 'block'
+  },
+
+  span: {
+    display: 'block'
+  },
+
+  button: {
+    display: 'block',
+    margin: '1em auto'
+  }
+});
 
 export default Title;
